a18990244254454dce79320fc1f730f158d9b8ef
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-12 17:39:02 -0500

    Add native dialogue detection command contract

---
+++ extension/reaadr_reaper/dialogue_detection_command.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+struct ReaProject;
+
+namespace reaadr::reaper {
+
+struct DialogueDetectionCommandResult {
+  bool cancelled = false;
+  std::size_t cue_count = 0;
+  std::string error;
+
+  explicit operator bool() const { return error.empty(); }
+};
+
+DialogueDetectionCommandResult run_dialogue_detection_command(
+  ReaProject* project = nullptr);
+
+} // namespace reaadr::reaper
