40befc433a1ce37ba3049bc1173c373b95d21e39
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-12 17:53:22 -0500

    Expose shared native recording host APIs

--- extension/reaadr_reaper/native_host_services.hpp
+++ extension/reaadr_reaper/native_host_services.hpp
@@ -3,6 +3,9 @@
 #include "dialogue_detection_adapter.hpp"
 #include "marker_snapshot_adapter.hpp"
 #include "overlay_refresh_adapter.hpp"
+#include "record_arm_adapter.hpp"
+#include "recording_setup_adapter.hpp"
+#include "recording_transport_executor.hpp"
 #include "render_artifact_adapter.hpp"
 #include "track_region_adapter.hpp"
 
@@ -22,7 +25,12 @@ TransactionApi native_transaction_api();
 MarkerSnapshotApi native_marker_snapshot_api();
 DialogueDetectionApi native_dialogue_detection_api();
 OverlayRefreshApi native_overlay_refresh_api();
+RecordArmApi native_record_arm_api();
+RecordingSetupApi native_recording_setup_api();
+RecordingTransportApi native_recording_transport_api();
 
+int native_play_state();
+double native_play_position();
 double native_project_frame_rate(ReaProject* project = nullptr);
 std::string native_project_cue_audio_path(ReaProject* project = nullptr);
 std::string native_utc_timestamp();
