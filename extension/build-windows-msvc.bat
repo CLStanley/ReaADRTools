c4123a8dfd8b8b505a1bf8ad20705ad35e1c482a
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-12 17:52:51 -0500

    Build native cue status service on Windows

--- extension/build-windows-msvc.bat
+++ extension/build-windows-msvc.bat
@@ -81,6 +81,7 @@ cl /nologo /EHsc /O2 /LD /std:c++17 ^
   reaadr_reaper\recording_transport_executor.cpp ^
   reaadr_reaper\overlay_refresh_adapter.cpp ^
   reaadr_reaper\cue_cleanup_adapter.cpp ^
+  app\cue_status_application_service.cpp ^
   app\recording_application_service.cpp ^
   app\recording_workflow_service.cpp ^
   app\overlay_application_service.cpp ^
