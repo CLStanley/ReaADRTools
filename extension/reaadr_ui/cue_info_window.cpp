bacd1f1d6852a48ca474f6e0a2f9c65eb18c6938
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-13 07:50:11 -0500

    Add Windows native Cue Info window

--- extension/reaadr_ui/cue_info_window.cpp
+++ extension/reaadr_ui/cue_info_window.cpp
@@ -42,6 +42,18 @@ constexpr int kTimer = 1;
 constexpr int kTransportPlayStop = 40044;
 constexpr int kMinWindowWidth = 820;
 constexpr int kMinWindowHeight = 560;
+#ifdef _WIN32
+constexpr int kLabelCue = 48500;
+constexpr int kLabelCharacter = 48501;
+constexpr int kLabelStatus = 48502;
+constexpr int kLabelType = 48503;
+constexpr int kLabelStart = 48504;
+constexpr int kLabelEnd = 48505;
+constexpr int kLabelDirection = 48506;
+constexpr int kLabelDialogue = 48507;
+constexpr int kLabelNotes = 48508;
+constexpr int kLabelJump = 48509;
+#endif
 
 CueInfoController* g_controller = nullptr;
 bool g_populating = false;
@@ -180,7 +192,63 @@ void close_window(HWND hwnd)
 {
   save_window_geometry(hwnd);
   KillTimer(hwnd, kTimer);
+#ifdef _WIN32
+  DestroyWindow(hwnd);
+#else
   EndDialog(hwnd, 0);
+#endif
+}
+
+void refresh_live_state(HWND hwnd)
+{
+  if (!g_controller || g_dirty) return;
+  const std::string previous = g_loaded_key;
+  if (g_controller->refresh()) {
+    if (g_controller->view().cue_key != previous) populate_editors(hwnd);
+    else update_live(hwnd);
+  } else {
+    SetDlgItemText(hwnd, kError, g_controller->error().c_str());
+  }
+}
+
+bool handle_cue_info_command(HWND hwnd, int command, int notification)
+{
+  if (!g_populating && editor_control(command) &&
+      (notification == EN_CHANGE || notification == CBN_SELCHANGE || notification == CBN_EDITCHANGE)) {
+    g_dirty = true;
+    SetDlgItemText(hwnd, kError, "Unsaved edit - choose Save to apply changes.");
+    return true;
+  }
+  if (!g_controller) return false;
+  if (command == kSave) {
+    if (g_controller->save(read_editors(hwnd))) {
+      if (g_close_on_save) {
+        close_window(hwnd);
+        return true;
+      }
+      refresh_character_choices(hwnd);
+      populate_editors(hwnd);
+    } else {
+      SetDlgItemText(hwnd, kError, g_controller->error().c_str());
+    }
+    return true;
+  }
+  if (command == kPrevious || command == kNext) {
+    const bool moved = command == kNext ? g_controller->next() : g_controller->previous();
+    if (moved) populate_editors(hwnd);
+    else SetDlgItemText(hwnd, kError, g_controller->error().c_str());
+    return true;
+  }
+  if (command == kJump) {
+    if (g_controller->jump_to_id(control_text(hwnd, kJumpId))) populate_editors(hwnd);
+    else SetDlgItemText(hwnd, kError, g_controller->error().c_str());
+    return true;
+  }
+  if (command == IDCANCEL) {
+    close_window(hwnd);
+    return true;
+  }
+  return false;
 }
 
 #ifndef _WIN32
@@ -205,15 +273,7 @@ INT_PTR cue_info_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM)
     }
 
     case WM_TIMER:
-      if (wparam == kTimer && g_controller && !g_dirty) {
-        const std::string previous = g_loaded_key;
-        if (g_controller->refresh()) {
-          if (g_controller->view().cue_key != previous) populate_editors(hwnd);
-          else update_live(hwnd);
-        } else {
-          SetDlgItemText(hwnd, kError, g_controller->error().c_str());
-        }
-      }
+      if (wparam == kTimer) refresh_live_state(hwnd);
       return 1;
 
     case WM_KEYDOWN:
@@ -223,46 +283,8 @@ INT_PTR cue_info_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM)
       }
       return 0;
 
-    case WM_COMMAND: {
-      const int command = LOWORD(wparam);
-      const int notification = HIWORD(wparam);
-      if (!g_populating && editor_control(command) &&
-          (notification == EN_CHANGE || notification == CBN_SELCHANGE || notification == CBN_EDITCHANGE)) {
-        g_dirty = true;
-        SetDlgItemText(hwnd, kError, "Unsaved edit - choose Save to apply changes.");
-        return 1;
-      }
-      if (!g_controller) return 0;
-      if (command == kSave) {
-        if (g_controller->save(read_editors(hwnd))) {
-          if (g_close_on_save) {
-            close_window(hwnd);
-            return 1;
-          }
-          refresh_character_choices(hwnd);
-          populate_editors(hwnd);
-        } else {
-          SetDlgItemText(hwnd, kError, g_controller->error().c_str());
-        }
-        return 1;
-      }
-      if (command == kPrevious || command == kNext) {
-        const bool moved = command == kNext ? g_controller->next() : g_controller->previous();
-        if (moved) populate_editors(hwnd);
-        else SetDlgItemText(hwnd, kError, g_controller->error().c_str());
-        return 1;
-      }
-      if (command == kJump) {
-        if (g_controller->jump_to_id(control_text(hwnd, kJumpId))) populate_editors(hwnd);
-        else SetDlgItemText(hwnd, kError, g_controller->error().c_str());
-        return 1;
-      }
-      if (command == IDCANCEL) {
-        close_window(hwnd);
-        return 1;
-      }
-      return 0;
-    }
+    case WM_COMMAND:
+      return handle_cue_info_command(hwnd, LOWORD(wparam), HIWORD(wparam)) ? 1 : 0;
 
     case WM_CLOSE:
       close_window(hwnd);
@@ -309,6 +331,151 @@ BEGIN
   LTEXT "", kError, 18, 566, 809, 48
 END
 SWELL_DEFINE_DIALOG_RESOURCE_END2(kDialog)
+#else
+
+constexpr const char* kCueInfoWindowClass = "ReaADRCueInfoWindow";
+
+void set_default_font(HWND control)
+{
+  if (!control) return;
+  SendMessage(control, WM_SETFONT,
+              reinterpret_cast<WPARAM>(GetStockObject(DEFAULT_GUI_FONT)), TRUE);
+}
+
+HWND create_child(HWND parent, const char* class_name, const char* text,
+                  DWORD style, int id)
+{
+  HWND child = CreateWindowExA(
+    0, class_name, text, WS_CHILD | WS_VISIBLE | style,
+    0, 0, 10, 10, parent, reinterpret_cast<HMENU>(static_cast<INT_PTR>(id)),
+    GetModuleHandleA(nullptr), nullptr);
+  set_default_font(child);
+  return child;
+}
+
+void layout_windows_controls(HWND hwnd)
+{
+  RECT client{};
+  if (!GetClientRect(hwnd, &client)) return;
+  const int width = client.right - client.left;
+  const int height = client.bottom - client.top;
+  const int right = (std::max)(780, width - 18);
+  const int full_width = (std::max)(300, right - 18);
+
+  SetWindowPos(GetDlgItem(hwnd, kHeader), nullptr, 18, 12, full_width, 22, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLiveMetrics), nullptr, 18, 38, full_width, 20, SWP_NOZORDER);
+
+  SetWindowPos(GetDlgItem(hwnd, kLabelCue), nullptr, 18, 70, 42, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kCueId), nullptr, 18, 90, 110, 24, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelCharacter), nullptr, 144, 70, 80, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kCharacter), nullptr, 144, 90, 190, 220, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelStatus), nullptr, 350, 70, 60, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kStatus), nullptr, 350, 90, 150, 180, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelType), nullptr, 516, 70, 60, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kCueType), nullptr, 516, 90, (std::max)(130, right - 516), 180, SWP_NOZORDER);
+
+  SetWindowPos(GetDlgItem(hwnd, kLabelStart), nullptr, 18, 124, 150, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kStart), nullptr, 18, 144, 170, 24, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelEnd), nullptr, 204, 124, 150, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kEnd), nullptr, 204, 144, 170, 24, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelDirection), nullptr, 390, 124, 80, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kDirection), nullptr, 390, 144, (std::max)(250, right - 390), 24, SWP_NOZORDER);
+
+  const int footer_top = (std::max)(500, height - 104);
+  const int notes_height = (std::max)(90, footer_top - 370);
+  SetWindowPos(GetDlgItem(hwnd, kLabelDialogue), nullptr, 18, 180, 80, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kDialogue), nullptr, 18, 200, full_width, 130, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kLabelNotes), nullptr, 18, 342, 80, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kNotes), nullptr, 18, 362, full_width, notes_height, SWP_NOZORDER);
+
+  const int action_y = footer_top + 8;
+  SetWindowPos(GetDlgItem(hwnd, kLabelJump), nullptr, 18, action_y + 4, 82, 18, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kJumpId), nullptr, 104, action_y, 120, 24, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kJump), nullptr, 232, action_y - 1, 72, 26, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kPrevious), nullptr, 326, action_y - 1, 86, 26, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kNext), nullptr, 418, action_y - 1, 72, 26, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kSave), nullptr, (std::max)(516, right - 190), action_y - 1, 86, 26, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, IDCANCEL), nullptr, (std::max)(610, right - 96), action_y - 1, 86, 26, SWP_NOZORDER);
+  SetWindowPos(GetDlgItem(hwnd, kError), nullptr, 18, action_y + 34, full_width, 44, SWP_NOZORDER);
+}
+
+LRESULT CALLBACK cue_info_window_proc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam)
+{
+  switch (message) {
+    case WM_CREATE:
+      create_child(hwnd, "STATIC", "", SS_LEFT, kHeader);
+      create_child(hwnd, "STATIC", "", SS_LEFT, kLiveMetrics);
+      create_child(hwnd, "STATIC", "Cue", SS_LEFT, kLabelCue);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_AUTOHSCROLL, kCueId);
+      create_child(hwnd, "STATIC", "Character", SS_LEFT, kLabelCharacter);
+      create_child(hwnd, "COMBOBOX", "", WS_TABSTOP | CBS_DROPDOWN | WS_VSCROLL, kCharacter);
+      create_child(hwnd, "STATIC", "Status", SS_LEFT, kLabelStatus);
+      create_child(hwnd, "COMBOBOX", "", WS_TABSTOP | CBS_DROPDOWN | WS_VSCROLL, kStatus);
+      create_child(hwnd, "STATIC", "Cue Type", SS_LEFT, kLabelType);
+      create_child(hwnd, "COMBOBOX", "", WS_TABSTOP | CBS_DROPDOWN | WS_VSCROLL, kCueType);
+      create_child(hwnd, "STATIC", "Start SMPTE / Seconds", SS_LEFT, kLabelStart);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_AUTOHSCROLL, kStart);
+      create_child(hwnd, "STATIC", "End SMPTE / Seconds", SS_LEFT, kLabelEnd);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_AUTOHSCROLL, kEnd);
+      create_child(hwnd, "STATIC", "Direction", SS_LEFT, kLabelDirection);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_AUTOHSCROLL, kDirection);
+      create_child(hwnd, "STATIC", "Dialogue", SS_LEFT, kLabelDialogue);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL, kDialogue);
+      create_child(hwnd, "STATIC", "Notes", SS_LEFT, kLabelNotes);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_MULTILINE | ES_AUTOVSCROLL | WS_VSCROLL, kNotes);
+      create_child(hwnd, "STATIC", "Jump to cue", SS_LEFT, kLabelJump);
+      create_child(hwnd, "EDIT", "", WS_BORDER | WS_TABSTOP | ES_AUTOHSCROLL, kJumpId);
+      create_child(hwnd, "BUTTON", "Jump", WS_TABSTOP | BS_PUSHBUTTON, kJump);
+      create_child(hwnd, "BUTTON", "Previous", WS_TABSTOP | BS_PUSHBUTTON, kPrevious);
+      create_child(hwnd, "BUTTON", "Next", WS_TABSTOP | BS_PUSHBUTTON, kNext);
+      create_child(hwnd, "BUTTON", "Save", WS_TABSTOP | BS_PUSHBUTTON, kSave);
+      create_child(hwnd, "BUTTON", "Close", WS_TABSTOP | BS_DEFPUSHBUTTON, IDCANCEL);
+      create_child(hwnd, "STATIC", "", SS_LEFT, kError);
+      fill_combo(hwnd, kCharacter, g_controller ? g_controller->character_choices() : std::vector<std::string>{});
+      fill_combo(hwnd, kStatus, core::cue_manager_status_choices());
+      fill_combo(hwnd, kCueType, core::cue_manager_type_choices());
+      layout_windows_controls(hwnd);
+      return 0;
+
+    case WM_SIZE:
+      layout_windows_controls(hwnd);
+      return 0;
+
+    case WM_GETMINMAXINFO: {
+      auto* info = reinterpret_cast<MINMAXINFO*>(lparam);
+      if (info) {
+        info->ptMinTrackSize.x = kMinWindowWidth;
+        info->ptMinTrackSize.y = kMinWindowHeight;
+      }
+      return 0;
+    }
+
+    case WM_TIMER:
+      if (wparam == kTimer) refresh_live_state(hwnd);
+      return 0;
+
+    case WM_COMMAND:
+      if (handle_cue_info_command(hwnd, LOWORD(wparam), HIWORD(wparam))) return 0;
+      break;
+
+    case WM_CLOSE:
+      close_window(hwnd);
+      return 0;
+  }
+  return DefWindowProc(hwnd, message, wparam, lparam);
+}
+
+bool register_windows_cue_info_class()
+{
+  WNDCLASSA window_class{};
+  window_class.lpfnWndProc = cue_info_window_proc;
+  window_class.hInstance = GetModuleHandleA(nullptr);
+  window_class.lpszClassName = kCueInfoWindowClass;
+  window_class.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  window_class.hbrBackground = reinterpret_cast<HBRUSH>(COLOR_BTNFACE + 1);
+  if (RegisterClassA(&window_class)) return true;
+  return GetLastError() == ERROR_CLASS_ALREADY_EXISTS;
+}
 #endif
 
 } // namespace
@@ -326,8 +493,56 @@ bool show_cue_info_window(CueInfoController& controller)
   g_close_on_save = false;
   return result >= 0;
 #else
-  (void)controller;
-  return false;
+  if (g_controller || !register_windows_cue_info_class()) return false;
+  g_controller = &controller;
+  g_dirty = false;
+  g_loaded_key.clear();
+  const CueInfoLaunchOptions launch = g_controller->consume_launch_options();
+  g_close_on_save = launch.close_on_save;
+
+  const CueInfoWindowLayout layout = g_controller->load_window_layout();
+  const int width = (std::max)(kMinWindowWidth, layout.width);
+  const int height = (std::max)(kMinWindowHeight, layout.height);
+  const int x = layout.has_position ? layout.x : CW_USEDEFAULT;
+  const int y = layout.has_position ? layout.y : CW_USEDEFAULT;
+  HWND owner = GetForegroundWindow();
+  HWND hwnd = CreateWindowExA(
+    WS_EX_DLGMODALFRAME,
+    kCueInfoWindowClass,
+    "ReaADR Cue Information",
+    WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_THICKFRAME,
+    x, y, width, height, owner, nullptr, GetModuleHandleA(nullptr), nullptr);
+  if (!hwnd) {
+    g_controller = nullptr;
+    g_close_on_save = false;
+    return false;
+  }
+
+  if (g_controller->refresh()) populate_editors(hwnd);
+  else SetDlgItemText(hwnd, kError, g_controller->error().c_str());
+  ShowWindow(hwnd, SW_SHOW);
+  UpdateWindow(hwnd);
+  restore_window_geometry(hwnd);
+  SetTimer(hwnd, kTimer, 100, nullptr);
+
+  MSG message{};
+  while (IsWindow(hwnd) && GetMessage(&message, nullptr, 0, 0) > 0) {
+    if (message.message == WM_KEYDOWN && message.wParam == VK_SPACE &&
+        !editor_has_focus(hwnd) && Main_OnCommand) {
+      Main_OnCommand(kTransportPlayStop, 0);
+      continue;
+    }
+    if (!IsDialogMessage(hwnd, &message)) {
+      TranslateMessage(&message);
+      DispatchMessage(&message);
+    }
+  }
+  const bool closed = !IsWindow(hwnd);
+  g_controller = nullptr;
+  g_close_on_save = false;
+  g_dirty = false;
+  g_loaded_key.clear();
+  return closed;
 #endif
 }
 
