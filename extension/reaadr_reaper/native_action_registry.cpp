81ad7f29db4ffea3db8c73edb35804b04261d7fc
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-14 18:57:24 -0500

    Register native quick action commands

--- extension/reaadr_reaper/native_action_registry.cpp
+++ extension/reaadr_reaper/native_action_registry.cpp
@@ -1,6 +1,7 @@
 #include "native_action_registry.hpp"
 
 #include "cue_info_action.hpp"
+#include "quick_action_action.hpp"
 #include "recording_action.hpp"
 
 namespace reaadr::reaper {
@@ -13,6 +14,11 @@ bool register_native_workflow_actions(reaper_plugin_info_t* plugin)
     unregister_recording_action(plugin);
     return false;
   }
+  if (!register_quick_action_actions(plugin)) {
+    unregister_cue_info_action(plugin);
+    unregister_recording_action(plugin);
+    return false;
+  }
   return true;
 }
 
@@ -21,6 +27,7 @@ void unregister_native_workflow_actions(reaper_plugin_info_t* plugin)
   if (!plugin) return;
   // Unregister in reverse order so partial registration and future additions
   // retain a predictable teardown sequence.
+  unregister_quick_action_actions(plugin);
   unregister_cue_info_action(plugin);
   unregister_recording_action(plugin);
 }
