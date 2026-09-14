9222ae561235e42cfdc98b3cb8a3b9148a778015
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-14 14:36:57 -0500

    Expose shared native overlay host services

--- extension/reaadr_reaper/native_host_services.hpp
+++ extension/reaadr_reaper/native_host_services.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "app/overlay_application_service.hpp"
 #include "cue_take_count_adapter.hpp"
 #include "dialogue_detection_adapter.hpp"
 #include "marker_snapshot_adapter.hpp"
@@ -27,6 +28,11 @@ MarkerSnapshotApi native_marker_snapshot_api();
 DialogueDetectionApi native_dialogue_detection_api();
 CueTakeCountApi native_cue_take_count_api();
 OverlayRefreshApi native_overlay_refresh_api();
+OverlayApplicationApi native_overlay_application_api();
+OverlaySelectionInput native_overlay_selection();
+bool native_overlay_refresh_callback(
+  const core::OverlayRefreshOptions& options,
+  std::string* error);
 RecordArmApi native_record_arm_api();
 RecordingSetupApi native_recording_setup_api();
 RecordingTransportApi native_recording_transport_api();
@@ -34,6 +40,7 @@ RecordingTransportApi native_recording_transport_api();
 int native_play_state();
 double native_play_position();
 double native_cursor_position();
+double native_current_project_frame_rate();
 double native_project_frame_rate(ReaProject* project = nullptr);
 std::string native_project_cue_audio_path(ReaProject* project = nullptr);
 std::string native_utc_timestamp();
