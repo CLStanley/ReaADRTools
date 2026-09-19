695d737ad2441a1285e7cfec59d7faa0cbfd4576
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-19 18:39:56 -0500

    Expose native Record Cue window shutdown

--- extension/reaadr_ui/recording_window.hpp
+++ extension/reaadr_ui/recording_window.hpp
@@ -8,5 +8,6 @@ class RecordingController;
 // Lua window remains installed as the parity reference until all platforms and
 // interactions have been smoke-tested against it.
 bool show_recording_window(RecordingController& controller);
+bool close_recording_window();
 
 } // namespace reaadr::ui
