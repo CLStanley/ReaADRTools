2b5873376a980f079ac613c366c9617df1134d55
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-19 19:10:36 -0500

    Add native Cue Info session shutdown

--- extension/reaadr_reaper/cue_info_command.hpp
+++ extension/reaadr_reaper/cue_info_command.hpp
@@ -5,5 +5,6 @@ namespace reaadr::reaper {
 // Opens the native Cue Info presentation. The Lua Cue Info Panel remains
 // installed and unchanged until behavior/UI/platform parity is smoke-tested.
 bool run_native_cue_info_command();
+bool shutdown_native_cue_info_command();
 
 } // namespace reaadr::reaper
