40ee62e13ae3ad1ce3d609f884106e7a4b1e2478
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-20 15:59:50 -0500

    Expose native recording timeline position to UI

--- extension/reaadr_ui/recording_controller.hpp
+++ extension/reaadr_ui/recording_controller.hpp
@@ -42,6 +42,7 @@ public:
   bool toggle_preroll_each_loop();
   bool retry_pending();
   bool shutdown();
+  double timeline_position() const { return context_.timeline_position(); }
 
   reaper::RecordingWindowLayout load_window_layout() const
   {
