64318ea8955e614bca48920f483cccb55742dd5a
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-20 19:10:50 -0500

    Add native ADR count-in transport intent

--- extension/reaadr_core/recording_transport.hpp
+++ extension/reaadr_core/recording_transport.hpp
@@ -33,6 +33,7 @@ struct RecordingTransportState {
   bool loop_range_active = false;
   bool operation_finalized = true;
   int take_count = 0;
+  int last_count_in_beat = 0;
 };
 
 struct RecordingTransportInput {
@@ -53,6 +54,7 @@ struct RecordingTransportActions {
   bool isolate_recording_track = false;
   bool play = false;
   bool record = false;
+  int play_count_in_beat = 0;
   bool restore_record_arm = false;
   bool refresh_active_cue = false;
   bool finalize_recorded_takes = false;
