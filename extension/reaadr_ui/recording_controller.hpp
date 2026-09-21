3e5a041b8d78028d4711cc379139e1be3fccb9e8
Charles Stanley <c.stanley.dev@gmail.com>
2026-09-21 10:52:17 -0500

    Remove obsolete recording beat-count presentation state

--- extension/reaadr_ui/recording_controller.hpp
+++ extension/reaadr_ui/recording_controller.hpp
@@ -17,7 +17,6 @@ struct RecordingViewState {
   double cue_end = 0.0;
   double preroll_seconds = 0.0;
   double countdown_seconds = 0.0;
-  int countdown_beats_remaining = 0;
   int count_in_beat = 0;
   int lane = 1;
   int take_count = 0;
